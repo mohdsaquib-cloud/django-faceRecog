491f37d7e8b0246ba4a65a05e990bc7999144106
Mohd Saquib <55183021+mohdsaquib-cloud@users.noreply.github.com>
2020-04-29 11:16:38 +0530

    jsj

--- recog/views.py
+++ recog/views.py
@@ -9,7 +9,6 @@ class VideoCamera(object):
         self.video = cv2.VideoCapture(0)
     def __del__(self):
         self.video.release()
-        cv2.destroyAllWindows()
     def get_frame(self):
         ret,frame= self.video.read()
         faces = face_cascade.detectMultiScale(frame, 1.3, 5)
